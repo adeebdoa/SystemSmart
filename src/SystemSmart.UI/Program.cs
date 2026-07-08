77ab63ad65d11cd5e6c37471bc779a693f0ea506
adeebdoa <adeebdoa@gmail.com>
2026-07-09 02:30:45 +0300

    Update Program.cs

--- src/SystemSmart.UI/Program.cs
+++ src/SystemSmart.UI/Program.cs
@@ -1,1 +1,24 @@
 
+using System;
+using System.Windows.Forms;
+
+namespace SystemSmart
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// نقطة بداية البرنامج.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+
+            Application.Run(new LoginForm());
+        }
+    }
+}
